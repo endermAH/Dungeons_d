fe413f367e1dbf2ddaa43c1c091a27c174f57a59
Evgeny Kuryatov <34351907+endermAH@users.noreply.github.com>
2020-07-15 21:31:25 +0300

    Feature/d-5 (#1)
    * MoveTo init
    * MoveTo component

---
+++ Assets/Scripts/Common/MoveTo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Common
+{
+    public class MoveTo : MonoBehaviour
+    {
+        public void MoveToPoint(Vector3 target, float duration)
+        {
+            var currentPosition = transform.position;
+            StartCoroutine(MoveFromTo(currentPosition, target, duration));
+        }
+
+        public void MoveToGameObject(GameObject target, float duration)
+        {
+            var currentPosition = transform.position;
+            var targetPosition = target.transform.position;
+            StartCoroutine(MoveFromTo(currentPosition, targetPosition, duration));
+        }
+
+        private IEnumerator MoveFromTo(Vector3 from, Vector3 to, float duration)
+        {
+            float timeElapsed = 0;
+
+            while (timeElapsed < duration)
+            {
+                transform.position = Vector3.Lerp(from, to, timeElapsed / duration);
+                timeElapsed += Time.deltaTime;
+
+                yield return null;
+            }
+
+            transform.position = to;
+        }
+    }
+}
