6ca5e30c81aa4c47bdf188a14d106f09cbc83be7
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2020-07-15 21:05:12 +0300

    MoveTo init

---
+++ Assets/Scripts/Common/MoveTo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class MoveTo : MonoBehaviour
+    {
+        public void MoveToPoint(Vector3 target, float time)
+        {
+            
+        }
+
+        public void MoveToGameObject(GameObject target, float time)
+        {
+            
+        }
+    }
+}
