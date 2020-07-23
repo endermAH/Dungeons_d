082d492f35fd8e33454198e2423fa354b71b3a44
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2020-07-23 21:54:22 +0300

    DUNG-5 - Add Enemy's behavior to MiniElf

---
+++ Assets/Scripts/Enemy/MiniElf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class MiniElf : Enemy
+    {
+        private void Start()
+        {
+            Stats = new EnemyStats(10,100);
+            currentHealth = Stats.MaxHealth;
+            UpdateHealthSlider();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
